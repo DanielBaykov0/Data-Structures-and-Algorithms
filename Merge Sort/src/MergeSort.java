8888ee1dcc007da019594c6ea2772936b048fbaf
Daniel Baykov <danibaikov@abv.bg>
2023-01-21 17:38:12 +0200

    initial Merge Sort

---
+++ Merge Sort/src/MergeSort.java
@@ -0,0 +1,17 @@
+public class MergeSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {20, 35, -15, 7, 55, 1, -22};
+
+
+    }
+
+    public static void mergeSort(int[] arr, int start, int end) {
+
+        if (end - start < 2) {
+            return;
+        }
+    }
+
+}
