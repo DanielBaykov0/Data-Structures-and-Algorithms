0f3352eb5c2532583d133708fd6586946cbecea0
DBaykov <danibaikov@abv.bg>
2023-01-21 18:38:18 +0200

    adding Merge Sort

--- Merge Sort/src/MergeSort.java
+++ Merge Sort/src/MergeSort.java
@@ -4,7 +4,11 @@ public class MergeSort {
 
         int[] arr = {20, 35, -15, 7, 55, 1, -22};
 
+        mergeSort(arr, 0, arr.length);
 
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 
     public static void mergeSort(int[] arr, int start, int end) {
@@ -12,6 +16,29 @@ public class MergeSort {
         if (end - start < 2) {
             return;
         }
+
+        int mid = (start + end) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid, end);
+        merge(arr, start, mid, end);
     }
 
+    public static void merge(int[] arr, int start, int mid, int end) {
+
+        if (arr[mid - 1] <= arr[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
+        }
+
+        System.arraycopy(arr, i, arr, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, arr, start, tempIndex);
+    }
 }
