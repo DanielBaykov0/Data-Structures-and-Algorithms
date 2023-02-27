76a5cbb1ccd702b13e25c687de72d39a151f74aa
Daniel Baykov <danibaikov@abv.bg>
2023-02-27 11:01:24 +0200

    adding Stack Challenge

--- Stacks/Stack/src/MainListStack.java
+++ Stacks/Stack/src/MainListStack.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class MainListStack {
 
     public static void main(String[] args) {
@@ -15,11 +17,44 @@ public class MainListStack {
         stack.push(mikeWilson);
         stack.push(billEnd);
 
-        stack.printStack();
+//        stack.printStack();
+//
+//        System.out.println(stack.peek());
+//
+//        System.out.println("Popped: " + stack.pop());
+//        System.out.println(stack.peek());
 
-        System.out.println(stack.peek());
+        // should return true
+        System.out.println(checkForPalindrome("abccba"));
+        // should return true
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw?"));
+        // should return true
+        System.out.println(checkForPalindrome("I did, did I?"));
+        // should return false
+        System.out.println(checkForPalindrome("hello"));
+        // should return true
+        System.out.println(checkForPalindrome("Don't nod"));
+    }
 
-        System.out.println("Popped: " + stack.pop());
-        System.out.println(stack.peek());
+    public static boolean checkForPalindrome(String string) {
+
+        Stack<Character> stack = new Stack<>();
+        StringBuilder stringNoPunctuation = new StringBuilder(string.length());
+        String lowerCase = string.toLowerCase();
+
+        for (int i = 0; i < lowerCase.length(); i++) {
+            char c = lowerCase.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                stringNoPunctuation.append(c);
+                stack.push(c);
+            }
+        }
+
+        StringBuilder reversedString = new StringBuilder(stack.size());
+        while (!stack.isEmpty()) {
+            reversedString.append(stack.pop());
+        }
+
+        return reversedString.toString().equals(stringNoPunctuation.toString());
     }
 }
