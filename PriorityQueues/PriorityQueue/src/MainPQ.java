28a438090e0ce95e17b4862dbf27918984c1c3b4
Daniel Baykov <danibaikov@abv.bg>
2023-03-01 19:42:10 +0200

    adding Priority Queue

---
+++ PriorityQueues/PriorityQueue/src/MainPQ.java
@@ -0,0 +1,35 @@
+import java.util.PriorityQueue;
+
+public class MainPQ {
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(25);
+        pq.add(-22);
+        pq.add(1343);
+        pq.add(54);
+        pq.add(0);
+        pq.add(-3492);
+        pq.add(429);
+
+//        System.out.println(pq.peek());
+//        System.out.println(pq.remove());
+//        System.out.println(pq.peek());
+//
+//        System.out.println(pq.poll());
+//        System.out.println(pq.peek());
+//
+//       System.out.println(pq.remove(54));
+//        System.out.println(pq.peek());
+
+        Object[] ints = pq.toArray();
+        for (Object num : ints) {
+            System.out.println(num);
+        }
+
+//        pq.add(-1);
+//        System.out.println(pq.peek());
+    }
+}
