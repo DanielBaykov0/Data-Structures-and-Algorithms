2c4bc19418106081311b665b0d2731efe2954e93
Daniel Baykov <danibaikov@abv.bg>
2023-03-01 19:20:37 +0200

    adding Max Heap Delete

--- Heaps/Heap/src/MainMaxHeap.java
+++ Heaps/Heap/src/MainMaxHeap.java
@@ -1,2 +1,22 @@
 public class MainMaxHeap {
+
+    public static void main(String[] args) {
+
+        MaxHeap maxHeap = new MaxHeap(10);
+
+        maxHeap.insert(80);
+        maxHeap.insert(75);
+        maxHeap.insert(60);
+        maxHeap.insert(68);
+        maxHeap.insert(55);
+        maxHeap.insert(40);
+        maxHeap.insert(52);
+        maxHeap.insert(67);
+
+        maxHeap.printHeap();
+
+        maxHeap.delete(0);
+
+        maxHeap.printHeap();
+    }
 }
